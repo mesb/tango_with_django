b654f54429ddfb60cbd6d112f26ba92d24ead800
M Brian <eng.mesembe@gmail.com>
2014-06-09 21:37:32 -0400

    Queried models and displayed data on template

--- rango/views.py
+++ rango/views.py
@@ -2,10 +2,13 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
+from .models import Category
+
 def index(request):
     context = RequestContext(request)
     
-    context_dict = {'boldmessage':  "I am a bold font from the context"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
     
     return render_to_response('rango/index.html', context_dict, context)
 
@@ -13,4 +16,4 @@ def index(request):
 def about(request):
     context = RequestContext(request)
     
-    return render_to_response('rango/about.html', context)
+    return render_to_response('rango/about.html', context_dict, context)
